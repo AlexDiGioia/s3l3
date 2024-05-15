d819dd727218314f55f8123261412a5949fb2d58
AlexDiGioia <digioia.alex2001@gmail.com>
2024-05-15 16:40:52 +0200

    agg

--- js/dom-manipulation.js
+++ js/dom-manipulation.js
@@ -1,7 +1,29 @@
-const form = document.getElementById("task-form")
+const form = document.getElementById("task-form");
 
 form.addEventListener("submit", function (event) {
-    event.preventDefault()
+  event.preventDefault();
 
-    const nameInputField = document.getElementById("task")
-})
+  const taskList = document.getElementById("task-list");
+  const task = document.getElementById("task");
+
+  const listElement = document.createElement("li");
+  listElement.innerText = task.value;
+  taskList.appendChild(listElement);
+
+  const btnListElement = document.createElement("button");
+  btnListElement.classList.add("btnListElement");
+  btnListElement.innerText = "Completata!";
+  listElement.appendChild(btnListElement);
+
+  const ListButtons = document.getElementsByClassName("btnListElement");
+  console.log(ListButtons);
+
+});
+
+const btnClickListElement = function (event) {
+  console.log("Hai cliccato un pulsante");
+  console.log("evento: ", event.target);
+};
+
+const ListButtons = document.getElementsByClassName("btnListElement");
+console.log(ListButtons);
