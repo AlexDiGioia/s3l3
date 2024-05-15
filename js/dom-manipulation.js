f64e9a12e364ffde3972174f5b4c08430672ac70
AlexDiGioia <digioia.alex2001@gmail.com>
2024-05-15 15:50:50 +0200

    es s3l3

---
+++ js/dom-manipulation.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("task-form")
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault()
+
+    const nameInputField = document.getElementById("task")
+})
